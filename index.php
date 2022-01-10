2d6dc29053f1f172a4f10f3f2e87beef3a8d6490
giulio30795 <giulioincarbone95@gmail.com>
2022-01-10 14:57:45 +0100

    inizio users

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/user.php';
+require_once __DIR__ . '/userPremium.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <header>
+        <h1>Shop-Online</h1>
+    </header>
+    <main>
+        
+    </main>
+
+</body>
+</html>
